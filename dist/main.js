5e888c81b0984392d344464769f5ce4231d218b9
bbggkkk <bbggkkk@naver.com>
2021-08-27 15:40:56 +0900

    dragStart dragEnd 추가

--- dist/main.js
+++ dist/main.js
@@ -218,7 +218,13 @@ __webpack_require__(1);
 (function () {
     var box = document.querySelector('#box');
     box === null || box === void 0 ? void 0 : box.gesture({
+        dragStart: function (parameter, ele, event) {
+            console.log("parameter :: ", parameter, "\nele :: ", ele, "\nevent :: ", event);
+        },
         drag: function (parameter, ele, event) {
+            console.log("parameter :: ", parameter, "\nele :: ", ele, "\nevent :: ", event);
+        },
+        dragEnd: function (parameter, ele, event) {
             console.log("parameter :: ", parameter, "\nele :: ", ele, "\nevent :: ", event);
         }
     });
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,13 @@ import './elementGesture';
     const box = document.querySelector('#box');
     
     box?.gesture({
+        dragStart : (parameter:any, ele:Object, event:any) => {
+            console.log("parameter :: ",parameter,"\nele :: ",ele,"\nevent :: ",event);
+        },
         drag : (parameter:any, ele:Object, event:any) => {
+            console.log("parameter :: ",parameter,"\nele :: ",ele,"\nevent :: ",event);
+        },
+        dragEnd : (parameter:any, ele:Object, event:any) => {
             console.log("parameter :: ",parameter,"\nele :: ",ele,"\nevent :: ",event);
         }
     });
